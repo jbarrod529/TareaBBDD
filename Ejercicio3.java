a5b41b998043a1c96e75c6d880b479520b6d0fbe
jbarrod529 <145765219+jbarrod529@users.noreply.github.com>
2023-10-06 13:34:33 +0200

    Create Ejercicio3.java

---
+++ Ejercicio3.java
@@ -0,0 +1,39 @@
+package daw1.comienzo;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+	
+		
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+	float num1;
+	float num2;
+	float num3;
+	float max1;
+	float maxTotal;
+	
+	System.out.println("Vamos a calcular el valor máximo de tres numeros que introduzcas, ¡comencemos!");
+	
+	System.out.println("Introduce el primer número");
+	
+	num1=scanner.nextFloat();
+	
+	System.out.println("Introduce el segundo número");
+	
+	num2=scanner.nextFloat();
+	
+	System.out.println("Introduce el tercer número número");
+	
+	num3=scanner.nextFloat();
+	
+	
+	max1=Math.max(num1, num2);
+	maxTotal=Math.max(num3, max1);
+	
+	
+	System.out.println("El valor máximo introducido es :"+maxTotal);
+	
+	
