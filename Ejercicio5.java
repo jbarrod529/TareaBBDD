1a71432e5d5e525d687139e77ed270ab0bf4b93f
jbarrod529 <145765219+jbarrod529@users.noreply.github.com>
2023-10-17 16:37:25 +0200

    Create Ejercicio5.java

---
+++ Ejercicio5.java
@@ -0,0 +1,48 @@
+package EntornoDesarrollo;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc= new Scanner(System.in);
+		
+		
+		int age;
+		
+		System.out.println("Introduce tu edad:");
+
+		age= sc.nextInt();
+		
+		while (age<=0) {
+			
+			System.out.println("La edad debe ser un numero positivo, por favor introduce de nuevo la edad:");	
+			
+			age= sc.nextInt();
+		}
+		
+		if(age<12) {
+			
+			System.out.println("Infante");
+		}
+		if(age>=12 && age<18) {
+			
+			System.out.println("Adolescente");
+		}
+		
+		if(age>=18 && age<=60) {
+			
+			System.out.println("Adulto");
+		}
+		
+		if(age>60) {
+			
+			System.out.println("Anciano");
+		}
+		
+	}
+	
+}
