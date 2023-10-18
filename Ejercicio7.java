8681bfc7ab4f67e714052b39ebbed58b289a9cbc
jbarrod529 <145765219+jbarrod529@users.noreply.github.com>
2023-10-18 17:15:09 +0200

    Create Ejercicio7.java

---
+++ Ejercicio7.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Ejercicio7 {
+	
+	public static void main (String[] arg) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int n;
+		int i;
+		int multiplicacion;
+		
+		System.out.print("Introduce un numero entero:");
+		n = sc.nextInt();
+		
+		
+		
+		for(i=1;i<=10;i++) {
+			
+			multiplicacion = n*i;
+		
+			
+		
+		
+		System.out.println(+ n + " x "  + i + " = "+  multiplicacion);
+		}	
+		
+	}
+
+}
